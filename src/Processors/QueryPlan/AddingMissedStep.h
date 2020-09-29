a0b1ad4de8ab8b3c3d4d68357dfffa6ef6061e36
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-09-29 19:21:58 +0300

    QueryPlan for StorageBuffer and StorageMaterializedView read.

---
+++ src/Processors/QueryPlan/AddingMissedStep.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <Processors/QueryPlan/ITransformingStep.h>
+
+namespace DB
+{
+
+struct ColumnDefault;
+using ColumnDefaults = std::unordered_map<std::string, ColumnDefault>;
+
+/// Convert one block structure to another. See ConvertingTransform.
+class AddingMissedStep : public ITransformingStep
+{
+public:
+    AddingMissedStep(const DataStream & input_stream_,
+                     Block result_header_,
+                     const ColumnDefaults & column_defaults_,
+                     const Context & context_);
+
+    String getName() const override { return "AddingMissed"; }
+
+    void transformPipeline(QueryPipeline & pipeline) override;
+
+    void describeActions(FormatSettings & settings) const override;
+
+private:
+    const ColumnDefaults column_defaults;
+    const Context & context;
+};
+
+}
